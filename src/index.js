6c338a9101e252e2317020121300d628a4e2ab4b
Aiden Luce <luceaiden4124@gmail.com>
2023-05-17 15:24:30 -0500

    props intro

--- src/index.js
+++ src/index.js
@@ -17,30 +17,34 @@ import './css/index.css'
 // const Person=()=><h2>John doe</h2>
 // const Message=()=><p>This is my message</p>
 
+
+
+const author = 'Abraham Verghese'
+const title = 'The Covenant of Water'
+const img = 'https://m.media-amazon.com/images/I/518+jXvFO6L._SY344_BO1,204,203,200_.jpg'
 function BookList(){
     return(
        <section className="bookList">
-           <Book/>
-           <Book/>
-           <Book/>
-           <Book/>
+           <Book author={author} title={title} img={img}/>
+           <Book author={author} title={title} img={img}/>
        </section>
     )
 }
 
 
-const Book=()=>{
+const Book=(props)=>{
+    console.log(props)
     return (
         <article className="book">
-            <Image/>
-            <Title/>
-            <Author/>
+            <img src={props.img} alt={props.title}/>
+            <h2>{props.title}</h2>
+            <h4>{props.author}</h4>
         </article>
     )
 }
-const Image=()=><img src='https://m.media-amazon.com/images/I/518+jXvFO6L._SY344_BO1,204,203,200_.jpg' alt='The Covenant of Water'/>
-const Title=()=><h2>The Covenant of Water</h2>
-const Author=()=><h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem'}}>Abraham Verghese</h4>
+// const Image=()=><img src='https://m.media-amazon.com/images/I/518+jXvFO6L._SY344_BO1,204,203,200_.jpg' alt='The Covenant of Water'/>
+// const Title=()=><h2>The Covenant of Water</h2>
+// const Author=()=><h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem'}}>Abraham Verghese</h4>
 
 
 
