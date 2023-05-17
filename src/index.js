4968ec707d8e936791c0fab01cfa574f249b7cef
Aiden Luce <luceaiden4124@gmail.com>
2023-05-17 15:35:59 -0500

    somewhat dynamic

--- src/index.js
+++ src/index.js
@@ -19,14 +19,14 @@ import './css/index.css'
 
 
 
-const author = 'Abraham Verghese'
-const title = 'The Covenant of Water'
-const img = 'https://m.media-amazon.com/images/I/518+jXvFO6L._SY344_BO1,204,203,200_.jpg'
+// const author = 'Abraham Verghese'
+// const title = 'The Covenant of Water'
+// const img = 'https://m.media-amazon.com/images/I/518+jXvFO6L._SY344_BO1,204,203,200_.jpg'
 function BookList(){
     return(
        <section className="bookList">
-           <Book author={author} title={title} img={img}/>
-           <Book author={author} title={title} img={img}/>
+           <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
+           <Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
        </section>
     )
 }
@@ -42,6 +42,20 @@ const Book=(props)=>{
         </article>
     )
 }
+
+const firstBook = {
+    author: 'Abraham Verghese',
+    title: 'The Covenant of Water',
+    img: 'https://m.media-amazon.com/images/I/518+jXvFO6L._SY344_BO1,204,203,200_.jpg'
+}
+const secondBook = {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg'
+}
+
+
+
 // const Image=()=><img src='https://m.media-amazon.com/images/I/518+jXvFO6L._SY344_BO1,204,203,200_.jpg' alt='The Covenant of Water'/>
 // const Title=()=><h2>The Covenant of Water</h2>
 // const Author=()=><h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem'}}>Abraham Verghese</h4>
