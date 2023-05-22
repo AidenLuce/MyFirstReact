3fb70264b683d51b7d298f92fa0ce2bb6bc5a0d0
Aiden Luce <luceaiden4124@gmail.com>
2023-05-22 10:40:23 -0500

    Components Feature

--- src/index.js
+++ src/index.js
@@ -72,10 +72,14 @@ const EventExamples =()=>{
 const Book=(props)=>{
     const {img, title, author} = props;
     // console.log(props)
+    const displayTitle = () =>{
+        console.log(title);
+    }
     return (
         <article className="book">
             <img src={img} alt={title}/>
             <h2>{title}</h2>
+            <button onClick={displayTitle}>display title</button>
             <h4>{author}</h4>
         </article>
     )
